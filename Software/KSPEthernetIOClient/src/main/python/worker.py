0cd592923c4f93783c2c7b9caa6ec7bc04aabd6f
microcontrollersig <melbpc.computerb@gmail.com>
2020-11-05 21:42:58 +1100

    Update worker.py

--- Software/KSPEthernetIOClient/src/main/python/worker.py
+++ Software/KSPEthernetIOClient/src/main/python/worker.py
@@ -7,19 +7,26 @@ import krpc
 IPADDRESS = '192.168.20.107'
 
 class KSP(QObject):
-    def __init__(self):
+    connected = QtCore.pyqtSignal(bool)
+    
+    def __init__(self, worker):
         super(KSP, self).__init__()        
         self.connection = None
+        self._worker = worker
+        self.connected.connect(self._worker.isConnected)
 
     def makeConnection(self):
         try:
-            print("Attempting to make connection...")
-            self.connection = krpc.connect(address=IPADDRESS, name="Mohan's GUI Test Program")
+            print("Attempting to make connection........")
+            #self.connection = krpc.connect(address=IPADDRESS, name="Mohan's GUI Test Program")
+            self.connection = krpc.connect(name="Mohan's GUI Test Program")
+            self.connected.emit(True)        
         except:
-            pass
+            self.connected.emit(False)
 
     def disconnect(self):
         self.connection.close()
+        self.connected.emit(False)
 
     def isConnected(self):
         return self.connection != None
@@ -37,17 +44,23 @@ class Worker(QObject):
     sasChanged = QtCore.pyqtSignal(bool)
     reportKSP = QtCore.pyqtSignal()
     connectKSP = QtCore.pyqtSignal()
+    disconnectKSP = QtCore.pyqtSignal()    
+    connectedStatus = QtCore.pyqtSignal(bool)
 
-    def __init__(self):
+
+    def __init__(self, ui):
         super(Worker, self).__init__()
+        self.ui = ui
+        self.connectedStatus.connect(self.ui.connectionStatusChanged)
         self.createKSPThread()
 
     def createKSPThread(self):
-        self._ksp = KSP()
+        self._ksp = KSP(self)
         self._ksp_thread = QtCore.QThread()
         self.sasChanged.connect(self._ksp.updateSAS)
         self.reportKSP.connect(self._ksp.reportFromKSP)
         self.connectKSP.connect(self._ksp.makeConnection)
+        self.disconnectKSP.connect(self._ksp.disconnect)      
         self._ksp.moveToThread(self._ksp_thread)
         self._ksp_thread.start()
         self.reportKSP.emit()
@@ -59,6 +72,13 @@ class Worker(QObject):
     @QtCore.pyqtSlot()     
     def makeConnection(self):
         self.connectKSP.emit()
+
+    def disconnectConnection(self):
+        self.disconnectKSP.emit()
+
+    @QtCore.pyqtSlot(bool)
+    def isConnected(self, val):
+        self.connectedStatus.emit(val)
 
     @QtCore.pyqtSlot(bool)
     def updateSAS(self, sas):
